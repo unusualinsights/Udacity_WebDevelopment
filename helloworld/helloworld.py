a0fe970d19a5cb80dc117acb83b6f8de43d9a291
unusualinsights <malayaleescholar@gmail.com>
2014-02-09 16:50:40 -0800

    Updated Problem Set 1, Lesson 1, and helloworld program

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -6,7 +6,7 @@ class MainPage( webapp2.RequestHandler ):
 
 	def get( self ):
 		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('Hello, Udacity!')
+		self.response.write('Hello, Udacity!!')
 
 
 application = webapp2.WSGIApplication( [
